c6d16210a53e6002272ba5d3e725263a3c146253
Guilherme <guilherme@gmail.com>
2020-03-25 16:20:59 -0300

    binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+  description = 'Wolf';
+  url = 'https://www.nwf.org/-/media/NEW-WEBSITE/Shared-Folder/Wildlife/Mammals/mammal_gray-wolf_600x300.ashx'
 }
